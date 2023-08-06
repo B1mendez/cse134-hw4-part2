c314d1a787397a67002553a90cd554ca331f5312
Brian Mendez <brianmendez2308@gmail.com>
2023-08-05 17:33:37 -0700

    finalized hw2

--- scripts/load-local.js
+++ scripts/load-local.js
@@ -1,29 +1,73 @@
-document.getElementById('loadLocal').addEventListener('click', loadLocalData);
-document.getElementById('loadRemote').addEventListener('click', loadRemoteData);
+function init() {
+  document.getElementById("loadLocal").addEventListener("click", loadLocalData);
+  document
+    .getElementById("loadRemote")
+    .addEventListener("click", loadRemoteData);
+}
 
 function loadLocalData() {
-  let localData = JSON.parse(localStorage.getItem('projectData'));
-  renderCards(localData);
+  let localData = JSON.parse(localStorage.getItem("projectData"));
+
+  if (localData) {
+    renderCards(localData);
+  } else {
+    alert("The following data ${localData} can not be found");
+  }
 }
 
 function loadRemoteData() {
-  fetch('https://your-remote-url.com/data.json') 
-    .then(response => response.json())
-    .then(data => renderCards(data))
-    .catch(error => console.error('Error:', error));
-}
+    const apiKey='$2b$10$yuR6cSSzXyiF1.WiwEx6Ne39Io7/0pnWfWSa/IQevIoAbtW/Ot/Yu'
+    const url='https://api.jsonbin.io/b/64cee8b89d312622a38c9203'
+    fetch(url,{
+    headers:{
+        'X-Master-Key': apiKey,
+    }
+    })
+    .then(response => {return response.json();})
+    .then(data => {renderCards(data.record);});
+  }
 
 function renderCards(data) {
-  let container = document.getElementById('cardContainer');
-  container.innerHTML = ''; // Clear container first
-  data.forEach(item => {
-    let card = document.createElement('project-card');
-    card.setAttribute('project-name', item.title);
-    card.setAttribute('img-src', item.imageSrc);
-    card.setAttribute('img-alt', item.imageAlt);
-    card.setAttribute('description', item.description);
-    card.setAttribute('link', item.link);
+  let container = document.getElementById("cardContainer");
+  container.innerHTML = "";
+  data.forEach((item) => {
+    let card = document.createElement("project-card");
+    card.setAttribute("project-name", item.title);
+    card.setAttribute("img-src", item.imageSrc);
+    card.setAttribute("img-alt", item.imageAlt);
+    card.setAttribute("description", item.description);
+    card.setAttribute("link", item.link);
     container.appendChild(card);
   });
 }
 
+function projectStorage() {
+  let projectExamples = [
+    {
+      title: "Project 1",
+      imageSrc: "/images/project1space.jpg",
+      imageAlt: "Image of space known as Wing of Orion!",
+      description: "This is a description for Project 1.",
+      link: "https://www.nasa.gov/image-feature/orion-s-recently-installed-solar-array-wings",
+    },
+    {
+      title: "Project 2",
+      imageSrc: "/images/project2porshe.jpg",
+      imageAlt: "An image of a porshe 911",
+      description: "This is a description for Project 2.",
+      link: "https://www.porsche.com/usa/",
+    },
+    {
+      title: "Project 3",
+      imageSrc: "/images/logo.PNG",
+      imageAlt: "An image of the Better Times Tutoring Logo",
+      description: "This is a description for Project 3.",
+      link: "https://bettertimestutoring.com/",
+    }
+  ];
+
+  let localProjectData = localStorage.setItem("projectData", JSON.stringify(projectExamples));
+
+  localStorage.setItem("projectData", localProjectData);
+}
+window.addEventListener("DOMContentLoaded", init);
